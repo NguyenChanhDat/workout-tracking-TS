943303723ae961ac3d907b7ac4522a40ba61435d
Dat Nguyen <thuong.nguyen@enouvo.com>
2025-01-15 14:14:08 +0700

    feat: update ITask, IUser Services

--- src/application/services/ITaskServices.ts
+++ src/application/services/ITaskServices.ts
@@ -3,9 +3,9 @@ import { UpdateTaskDto } from '../dto/task/updateTaskDto'
 import { Task } from '../../domain/entities/Task'
 
 export interface ITaskServices {
-    createEntity(task: CreateTaskDto): Promise<Task>
-    updateEntity(task: Task, updateInfor: UpdateTaskDto): Promise<Task>
-    deleteEntity(taskById: Task): Promise<void>
+    createEntity(task: CreateTaskDto): Promise<void>
+    updateEntity(taskId: number, updateInfor: UpdateTaskDto): Promise<void>
+    deleteEntity(taskId: number): Promise<void>
     getEntityById(taskId: number): Promise<Task | null>
     showListEntity(): Promise<Task[] | null>
     getByUserId(userId: number): Promise<Task | null>
--- src/application/services/IUserServices.ts
+++ src/application/services/IUserServices.ts
@@ -3,9 +3,9 @@ import { UpdateUserDto } from '../dto/user/updateUserDto'
 import { User } from '../../domain/entities/User'
 
 export interface IUserServices {
-    createEntity(user: CreateUserDto): Promise<User>
-    updateEntity(user: User, updateInfo: UpdateUserDto): Promise<User>
-    deleteEntity(user: User): Promise<void>
+    createEntity(user: CreateUserDto): Promise<void>
+    updateEntity(userId: number, updateInfo: UpdateUserDto): Promise<void>
+    deleteEntity(userId: number): Promise<void>
     getUserById(userId: number): Promise<User | null>
     getUserByUsername(username: string): Promise<User | null>
     showListEntity(): Promise<User[] | null>
