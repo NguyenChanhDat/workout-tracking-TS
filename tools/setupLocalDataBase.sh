d7fc5e08617cc852d60fc8a1316af54e21f6007e
Dat Nguyen <thuong.nguyen@enouvo.com>
2025-01-15 22:21:25 +0700

    feat: add kill old docker resources for shell script file

--- tools/setupLocalDataBase.sh
+++ tools/setupLocalDataBase.sh
@@ -1,3 +1,26 @@
+#!/bin/bash
+set -e
+
+h1() {
+  local BOLD="\033[1m"
+  local FG_WHITE="\033[97m"        
+  local BG_BLUE="\033[48;5;33m"  
+  local RS="\033[0m"              
+  local MSG=$1
+  echo -e "${BG_BLUE}${FG_WHITE}${BOLD} [$(date +"%H:%M:%S")] ${MSG} ${RS}"
+}
+
+
+
+h1 "Kill Old Container"
+docker kill fb-db-local || true
+
+h1 "Delete Old Container"
+docker rm fb-db-local || true
+
+h1 "Delete Old Image"
+docker rmi fb-db-local || true
+ 
 h1 "Build DB Image"
 docker build --progress=plain -t fb/db-local -f Dockerfile.db .
 
