2eee2fba05558b56ea834f9a2e1db4e3e30f2598
NguyenChanhDat <chanhdat2504@gmail.com>
2025-01-26 23:56:26 +0700

    feat: add typeorm bootstrap, config

--- src/infra/databases/config/config.ts
+++ src/infra/databases/config/config.ts
@@ -1,3 +1,5 @@
+import { DataSource } from 'typeorm';
+
 export const MssqlConfig = {
   user: process.env.DB_USER || 'sa',
   password: process.env.DB_PASSWORD || 'Dat20112011',
@@ -7,3 +9,15 @@ export const MssqlConfig = {
     trustServerCertificate: true,
   },
 };
+
+export const typeormConfig = new DataSource({
+  type: 'mssql',
+  host: process.env.DB_SERVER || 'localhost',
+  port: Number(process.env.DB_PORT) || 1401,
+  username: process.env.DB_USER || 'sa',
+  password: process.env.DB_PASSWORD || 'Dat20112011',
+  database: process.env.DB_DATABASE || 'LocalDatabase',
+  options: {
+    trustServerCertificate: true,
+  },
+});
--- src/infra/databases/dataSource/BootstrapTypeOrm.ts
+++ src/infra/databases/dataSource/BootstrapTypeOrm.ts
@@ -1,23 +1,12 @@
 import { IBootstrap } from '../../../presentation/bootstrap/IBootstrap';
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
+import { typeormConfig } from '../config/config';
+
+export const appDataSource: DataSource = typeormConfig;
 
 export class BootstrapTypeOrm implements IBootstrap {
-  private appDataSource: DataSource = new DataSource({
-    type: 'mssql',
-    host: 'localhost',
-    port: 1401,
-    username: 'sa',
-    password: 'Dat20112011',
-    database: 'ABC',
-    options: {
-      trustServerCertificate: true,
-    },
-  });
-  public dataSourceIsInitialized = async (): Promise<boolean> => {
-    return this.appDataSource.isInitialized;
-  };
   public initialize = async (): Promise<void> => {
-    await this.appDataSource.initialize();
+    await appDataSource.initialize();
   };
 }
