2dbe63adbf04e017e5740bec472d4476fabdb577
NguyenChanhDat <chanhdat2504@gmail.com>
2025-01-27 22:40:34 +0700

    refactor: remove all unused imports

--- src/domain/repositories/ITaskRepository.ts
+++ src/domain/repositories/ITaskRepository.ts
@@ -1,7 +1,5 @@
-import { UpdateTaskDto } from '../../application/dto/task/updateTaskDto'
 import { Task } from '../entities/Task'
 import { EntityRepository } from './EntityRepository'
-import { CreateTaskDto } from '../../application/dto/task/createTaskDto'
 
 export interface ITaskRepository extends EntityRepository<Task> {
     // createEntity(task: CreateTaskDto): Promise<void>
--- src/domain/repositories/IUserRepository.ts
+++ src/domain/repositories/IUserRepository.ts
@@ -1,7 +1,5 @@
-import { CreateUserDto } from '../../application/dto/user/createUserDto'
 import { User } from '../entities/User'
 import { EntityRepository } from './EntityRepository'
-import { UpdateUserDto } from '../../application/dto/user/updateUserDto'
 
 export interface IUserRepository extends EntityRepository<User> {
     // getEntityById(id: number): Promise<User|null>
