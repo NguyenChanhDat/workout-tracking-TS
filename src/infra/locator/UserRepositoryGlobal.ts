87e88e08ed4fb7f6575c3e2ebdc0d64f8c923654
Dat Nguyen <chanhdat2504@gmail.com>
2025-04-02 10:30:51 +0700

    feat: add missing jest npm, delete surplus testcases

--- src/infra/locator/UserRepositoryGlobal.ts
+++ src/infra/locator/UserRepositoryGlobal.ts
@@ -1,5 +1,5 @@
+import { UserTypeOrmRepository } from '@infra/databases/repositories/typeOrm/UserTypeOrmRepository';
 import { IUserRepository } from '../../domain/repositories/IUserRepository';
-import { UserInMemoryRepository } from '../databases/repositories/inMemory/UserInMemoryRepository';
 
 export const userRepositoryGlobal: IUserRepository =
-  new UserInMemoryRepository();
+  new UserTypeOrmRepository();
