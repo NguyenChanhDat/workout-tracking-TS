69e6ac009203b0870a17d4cfcf5a4ed439c3c195
Dat Nguyen <chanhdat2504@gmail.com>
2025-04-18 22:57:45 +0700

    feat: add cors

--- src/presentation/rest/app.ts
+++ src/presentation/rest/app.ts
@@ -1,11 +1,14 @@
-import express from 'express'
-import { UserRoutes } from './routes/UserRoutes'
-const app = express()
+import express from 'express';
+import { UserRoutes } from './routes/UserRoutes';
+import cors from 'cors';
 
+const app = express();
+
+app.use(cors());
 export const rest = (PORT: number) => {
-    app.use(express.json())
-    app.use('/user', UserRoutes)
-    app.listen(PORT, () => {
-        console.log(`Server running at http://localhost:${PORT}`)
-    })
-}
+  app.use(express.json());
+  app.use('/user', UserRoutes);
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
+};
